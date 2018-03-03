d457f19b8a5d7dfb1e37f084506255941f818920
Lea Verou <lea@verou.me>
2018-03-03 16:56:30 -0500

    Quick eval, pretty print of data, closest node info

---
+++ helpers.js
@@ -0,0 +1,60 @@
+function formatObject(obj, o = {}, label) {
+	var type = $.type(obj);
+
+	if (type == "object" || type == "array") {
+		var length = Object.keys(obj).length;
+
+		if (length === 0) {
+			return $.create({tag: "em", textContent: `(Empty ${type})`, className: "type-empty"});
+		}
+
+		var ret = $.create("dl", {className: "type-" + type});
+
+		var flags = {...o};
+
+		if (o.dataLabels && label) {
+			flags.isData = flags.isData || o.dataLabels.includes(label);
+		}
+
+		for (let label in obj) {
+			let value = obj[label];
+
+			$.contents(ret, [
+				{tag: "dt", innerHTML: label},
+				{tag: "dd", contents: formatObject(value, flags, label)}
+			]);
+		}
+
+		if (flags.isData) {
+			// Should it be collapsible?
+			var collapsible = JSON.stringify(obj).length > 200;
+
+			if (collapsible) {
+				var summary = `${capitalize(type)} (${length} ${type == "object"? "Properties" : "Items"})`;
+				ret = $.create("details", {
+					contents: [
+						{tag: "summary", textContent: summary},
+						ret
+					]
+				});
+			}
+		}
+
+		return ret;
+	}
+
+	// Primitive
+	return o.isData? formatPrimitive(obj) : obj;
+}
+
+function capitalize(str) {
+	return str[0].toUpperCase() + str.slice(1);
+}
+
+function formatPrimitive(value) {
+	if (value === null) {
+		return $.create({tag: "em", textContent: "(Empty)", className: "type-empty"});
+	}
+
+	return JSON.stringify(value);
+}
--- sidebar.js
+++ sidebar.js
@@ -1,39 +1,143 @@
 var $ = Bliss, $$ = $.$;
 
+chrome.devtools.panels.elements.onSelectionChanged.addListener(async function() {
+	updateSidebar();
+});
+
+async function updateSidebar() {
+	var hasMavo = await eval("typeof Mavo !== 'undefined'");
+
+	if (!hasMavo) {
+		return;
+	}
+
+	try {
+		var info = await eval(`(${getInfo})($0)`);
+
+		render(info);
+	}
+	catch (e) {
+		console.error(e);
+		showError(e);
+	}
+}
+
+function eval(code) {
+	return new Promise((resolve, reject) => {
+		chrome.devtools.inspectedWindow.eval(code, (result, exception) => {
+			if (exception) {
+				reject(exception);
+			}
+			else {
+				resolve(result);
+			}
+		});
+	});
+}
+
+// Beware: This runs in the context of the page!!
+function getInfo(element) {
+	var node = Mavo.Node.get(element);
+	var closestNode = node || Mavo.Node.getClosest(element);
+	var ret = {};
+
+	function getNodeInfo(node) {
+		return {
+			"Property": node.property,
+			"Type": node.nodeType,
+			"Data": JSON.parse(Mavo.safeToJSON(node.liveData))
+		};
+	}
+
+	if (node) {
+		ret["Node"] = getNodeInfo(node);
+	}
+	else if (closestNode) {
+		ret["Closest Node"] = getNodeInfo(closestNode);
+	}
+
+	if (closestNode) {
+		var expressions = Mavo.DOMExpression.search(element);
+
+		ret.Expressions = {};
+
+		if (expressions && expressions.length) {
+			for (let e of expressions) {
+				var value = e.value.length === 1? e.value[0] : e.value;
+				ret.Expressions[e.attribute || '<em class="de-emphasized">(Text Content)</em>'] = {
+					Template: e.expression,
+					"Current value": JSON.parse(Mavo.safeToJSON(value))
+				};
+			}
+		}
+	}
+
+	return ret;
+}
+
+// Beware: This runs in the context of the page!!
+function quickEval(element, code) {
+	var node = Mavo.Node.getClosest(element).group;
+	var data = node.getData({live: true});
+	var expression = new Mavo.Expression(code);
+	var value = JSON.parse(Mavo.safeToJSON(expression.eval(data)));
+	console.log(code, "=", value);
+	return value;
+}
+
 function render(info) {
 	document.body.textContent = "";
 	document.body.classList.remove("error");
 
 	for (var header in info) {
-		var details = $.create("details", {
+		var dl, details = $.create("details", {
 			open: true,
 			contents: [
 				{tag: "summary", textContent: header},
-				objectToDl(info[header])
+				dl = formatObject(info[header], {
+					dataLabels: ["Data", "Current value"]
+				})
 			]
 		});
 
+		if (header == "Expressions") {
+			var qe = $.contents(document.createDocumentFragment(), [
+				{tag: "dt", textContent: "Quick eval"},
+				{
+					tag: "dd",
+					contents: {
+						tag: "form",
+						className: "quick-eval",
+						contents: [
+							{tag: "input", name: "expression"},
+							{tag: "button", textContent: "Run"}
+						],
+						events: {
+							submit: function(evt) {
+								evt.preventDefault();
+								var dd = this.parentNode;
+								var output = $("output", dd) || $.create("output", {inside: dd});
+
+								eval(`(${quickEval})($0, "${this.expression.value}")`).then(value => {
+									output.textContent = "";
+									output.append(formatObject(value, {isData: true}));
+								});
+							}
+						}
+					}
+				}
+			]);
+
+			dl.prepend(qe);
+		}
+
 		document.body.append(details);
 	}
-}
-
-function objectToDl(obj) {
-	var dl = $.create("dl");
-
-	for (let label in obj) {
-		var value = obj[label];
-		var contents = $.type(value) == "object"? objectToDl(value) : value;
-
-		$.contents(dl, [
-			{tag: "dt", innerHTML: label},
-			{tag: "dd", contents: contents}
-		]);
-	}
-
-	return dl;
 }
 
 function showError(e) {
 	document.body.classList.add("error");
 	document.body.textContent = e;
 }
+
+onload = updateSidebar;
